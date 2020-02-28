8c43a257d441d6f771a11ddb84fed07c982a8e50
Raúl Montón <raul_m_p@me.com>
2020-02-28 11:23:34 +0100

    Removed unused gestureRecognized handler

--- CellTransport/GameViewController.swift
+++ CellTransport/GameViewController.swift
@@ -315,11 +315,7 @@ class GameViewController: UIViewController {
         
         // configure the view
         scnView.backgroundColor = UIColor.black
-        
-        // add a tap gesture recognizer
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
-        scnView.addGestureRecognizer(tapGesture)
-        
+                
         // finish UI configuration
         //nparticlesTextField.text = String(nbodies)
         buttonContainerView.layer.cornerRadius = 7.5
@@ -416,42 +412,7 @@ class GameViewController: UIViewController {
         scnView.delegate = self
         
     }
-    
-    @objc
-    func handleTap(_ gestureRecognize: UIGestureRecognizer) {
-        // retrieve the SCNView
-        
-        // check what nodes are tapped
-        let p = gestureRecognize.location(in: scnView)
-        let hitResults = scnView.hitTest(p, options: [:])
-        // check that we clicked on at least one object
-        if hitResults.count > 0 {
-            // retrieved the first clicked object
-            let result = hitResults[0]
-            
-            // get its material
-            let material = result.node.geometry!.firstMaterial!
-            
-            // highlight it
-            SCNTransaction.begin()
-            SCNTransaction.animationDuration = 0.5
-            
-            // on completion - unhighlight
-            SCNTransaction.completionBlock = {
-                SCNTransaction.begin()
-                SCNTransaction.animationDuration = 0.5
-                
-                material.emission.contents = UIColor.black
-                
-                SCNTransaction.commit()
-            }
-            
-            material.emission.contents = UIColor.red
-            
-            SCNTransaction.commit()
-        }
-    }
-    
+   
     override var shouldAutorotate: Bool {
         return true
     }
